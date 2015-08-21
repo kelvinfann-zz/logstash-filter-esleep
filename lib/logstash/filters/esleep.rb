d24008e77527b2214b8a399a1976cee7e5c21564
kelvin fann <kelvin.fann@centro.net>
2015-08-21 14:43:21 -0700

    refactored

--- lib/logstash/filters/esleep.rb
+++ lib/logstash/filters/esleep.rb
@@ -21,72 +21,76 @@ class LogStash::Filters::Esleep < LogStash::Filters::Base
   #     filter {
   #       sleep {
   #         # Sleep 1 second for every event.
-  #         time => "1"
+  #         sleeptime => "1"
   #       }
   #     }
-  config :time, :validate => :string
+  config :sleeptime, :validate => :string
 
   # Sleep on every N'th. This option is ignored in replay mode.
   #
   # Example:
   # [source,ruby]
   #     filter {
-  #       sleep {
-  #         time => "1"   # Sleep 1 second
+  #       esleep {
+  #         sleeptime => "1"   # Sleep 1 second
   #         every => 10   # on every 10th event
   #       }
   #     }
   config :every, :validate => :number, :default => 1
 
+
+  # Forces sleep after a certain amount of elapsed time if 
+  # hasn't reached n'th amount.
+  #
+  # Example:
+  # [source,ruby]
+  #     filter {
+  #       esleep {
+  #         sleeptime => "1"   # Sleep 1 second
+  #         every => 10   # on every 10th event
+  #         timelimt => 10 # or sleeps every 10 seconds
+  #       }
+  #     }
   config :timelimit, :validate => :number, :default => 0
 
   public
   def register
     require "atomic"
-    if @time.nil?
-      raise ArgumentError, "Missing required parameter 'time' for input/eventlog"
-    end
     @count = Atomic.new(0)
     @elapsed_time = Atomic.new(0.0)
-    @timelimit = - @timelimit
-    @last_sleep_time = Time.now.to_i
+    @check_time = @timelimit == 0
+    @timelimit = @timelimit
   end # def register
 
   public
   def filter(event)
     return unless filter?(event)
     @count.update {|v| v + 1 }
-    clock = event.timestamp.to_f
-    if @last_clock
-        @elapsed_time.update { |v| v - (clock - @last_clock) } #stores elapsed_time as a negative
-    end
-    if @elapsed_time.value <= @timelimit
-      est_curr_time = @last_sleep_time - @timelimit
-      act_curr_time = Time.now.to_i
-      if est_curr_time > act_curr_time
-        @elapsed_time.update { |v|  - (act_curr_time - @last_sleep_time) }
-      else
-        start_sleep(event)
-      end
-    end
     if @count.value >= @every
       start_sleep(event)
     end
-    @last_clock = clock
     filter_matched(event)
   end # def filter
 
+  public
+  def flush(options = {})
+    @elapsed_time.update {|v| v + 5}
+    if @timelimit >= @elapsed_time.value && @check_time
+      start_sleep(event)
+    end
+    return
+  end
+
   def start_sleep(event)
-    case @time
-      when Fixnum, Float; time = @time
+    # This case statement is legacy from the original sleep code
+    case @sleeptime
+      when Fixnum, Float; sleeptime = @sleeptime
       when nil;
-      else; time = event.sprintf(@time).to_f
+      else; sleeptime = event.sprintf(@sleeptime).to_f
     end
     @elapsed_time.update { |v| 0 } 
     @count.update { |v| 0 }
-    @last_clock = nil
-    @logger.debug? && @logger.debug("Sleeping", :delay => time)
-    @last_sleep_time = Time.now.to_i + time
-    sleep(time)
+    @logger.debug? && @logger.debug("Sleeping", :delay => sleeptime)
+    sleep(sleeptime)
   end # reset
 end # class LogStash::Filters::Sleep
